1df80f06cb98544173494e872533f162406f395d
SergeyLebidko <it.sergeyler@mail.ru>
2021-06-28 13:03:26 +0300

    Создана заготовка для компонента с календарем лиги

---
+++ src/CompetitionCalendar/CompetitionCalendar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './CompetitionCalendar.module.css';
+
+function CompetitionCalendar(){
+    return (
+        <div>
+            Здесь будет календарь лиги
+        </div>
+    );
+}
+
+export default CompetitionCalendar;
