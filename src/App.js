ef5430d5f1d20e52c4300b6b0bd991c5042e001b
SergeyLebidko <it.sergeyler@mail.ru>
2021-06-28 11:10:11 +0300

    Добавлен роут на основные компоненты

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import {HashRouter, Switch, Route} from "react-router-dom";
+import MainPage from './MainPage/MainPage';
 import './App.css';
 
 function App() {
     return (
         <HashRouter>
             <Switch>
-                <Route exact path="/" component={Cap}/>
+                <Route exact path="/" component={MainPage}/>
+                <Route path="/competition_list" component={Cap}/>
+                <Route path="/team_list" component={Cap}/>
+                <Route path="/competition_calendar" component={Cap}/>
+                <Route path="/team_calendar" component={Cap}/>
             </Switch>
         </HashRouter>
     );
---
+++ src/MainPage/MainPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './MainPage.module.css';
+
+function MainPage() {
+    return (
+        <div>
+            Здесь будет главная страница сайта
+        </div>
+    )
+}
+
+export default MainPage;
