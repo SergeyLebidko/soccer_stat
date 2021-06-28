41759527c1c83ad6bead31fc484a355ac2d48f43
SergeyLebidko <it.sergeyler@mail.ru>
2021-06-28 16:56:02 +0300

    Добавлен каркас для работы с хранилищем

---
+++ src/store/store.js
@@ -0,0 +1,7 @@
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+
+
+// let store = createStore(combinedReducer, {}, applyMiddleware(thunk));
+
+// export default store;
