80e47177b88688a3e4a959533fca90c494c15aec
SergeyLebidko <it.sergeyler@mail.ru>
2021-06-28 13:01:12 +0300

    Создана заготовка для компонента со списком команд

---
+++ src/TeamList/TeamList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './TeamList.module.css';
+
+function TeamList(){
+    return (
+        <div>
+            Здесь будем список команд
+        </div>
+    );
+}
+
+export default TeamList;
