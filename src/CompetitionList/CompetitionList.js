2c3f5cea04f474b7d2333d60698dde5dc95517a9
SergeyLebidko <it.sergeyler@mail.ru>
2021-06-28 12:58:45 +0300

    Создана заготовка для компонента со список м лиг

---
+++ src/CompetitionList/CompetitionList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './CompetitionList.module.css';
+
+function CompetitionList(){
+    return (
+        <div>
+            Здесь будет список лиг
+        </div>
+    );
+}
+
+export default CompetitionList;
