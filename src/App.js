f3d2c06ff90d6893cbaf7fe52fce23f3b86a8bd3
SergeyLebidko <it.sergeyler@mail.ru>
2021-06-28 18:42:33 +0300

    Удалены ненужные компоненты

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
-import MainPage from './MainPage/MainPage';
-import CompetitionList from './CompetitionList/CompetitionList';
-import TeamList from './TeamList/TeamList';
-import CompetitionCalendar from './CompetitionCalendar/CompetitionCalendar';
-import TeamCalendar from './TeamCalendar/TeamCalendar';
+
 import style from './App.module.css';
 
 function App() {
@@ -12,11 +8,6 @@ function App() {
         <div className={style.container}>
             <h1 className={style.site_title}>Soccer Stat</h1>
             <Switch>
-                <Route exact path="/" component={MainPage}/>
-                <Route path="/competition_list" component={CompetitionList}/>
-                <Route path="/team_list" component={TeamList}/>
-                <Route path="/competition_calendar" component={CompetitionCalendar}/>
-                <Route path="/team_calendar" component={TeamCalendar}/>
             </Switch>
         </div>
     );
