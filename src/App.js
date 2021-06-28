8f790e8d900a8204a666c76aefee8ecb09ac59e0
SergeyLebidko <it.sergeyler@mail.ru>
2021-06-28 18:47:12 +0300

    Добавлен компонент Competitions.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import {Switch, Route} from 'react-router-dom';
-
+import {Switch, Route, Redirect} from 'react-router-dom';
+import Competitions from './Competitions/Competitions';
 import style from './App.module.css';
+
 
 function App() {
     return (
         <div className={style.container}>
             <h1 className={style.site_title}>Soccer Stat</h1>
             <Switch>
+                <Route exact path="/">
+                    <Redirect to="/competitions"/>
+                </Route>
+                <Route path="/competitions" component={Competitions}/>
             </Switch>
         </div>
     );
---
+++ src/Competitions/Competitions.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './Competitions.module.css';
+
+function Competitions() {
+    return (
+        <div>
+            Здесь будет список лиг
+        </div>
+    )
+}
+
+export default Competitions;
