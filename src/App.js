f160d486f9c72df3e8008a98f2343324ebc49094
SergeyLebidko <it.sergeyler@mail.ru>
2021-06-28 13:07:05 +0300

    Заготовки компонентов добавлены в роутер. Удален компонент-заглушка

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 import Navigation from './Navigation/Navigation';
 import MainPage from './MainPage/MainPage';
+import CompetitionList from './CompetitionList/CompetitionList';
+import TeamList from './TeamList/TeamList';
+import CompetitionCalendar from './CompetitionCalendar/CompetitionCalendar';
+import TeamCalendar from './TeamCalendar/TeamCalendar';
 import './App.css';
+
 
 function App() {
     return (
@@ -11,23 +16,13 @@ function App() {
             <Navigation/>
             <Switch>
                 <Route exact path="/" component={MainPage}/>
-                <Route path="/competition_list" component={Cap}/>
-                <Route path="/team_list" component={Cap}/>
-                <Route path="/competition_calendar" component={Cap}/>
-                <Route path="/team_calendar" component={Cap}/>
+                <Route path="/competition_list" component={CompetitionList}/>
+                <Route path="/team_list" component={TeamList}/>
+                <Route path="/competition_calendar" component={CompetitionCalendar}/>
+                <Route path="/team_calendar" component={TeamCalendar}/>
             </Switch>
         </>
     );
 }
 
 export default App;
-
-
-// Временный компонент-заглушка
-function Cap() {
-    return (
-        <div>
-            Раздел находится в разработке
-        </div>
-    )
-}
