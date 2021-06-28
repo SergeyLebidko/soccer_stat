b769a8b22361eb5a85c993e5803cd861d4ec1893
SergeyLebidko <it.sergeyler@mail.ru>
2021-06-28 13:19:32 +0300

    Реализовано переключение между компонентами по ссылкам навигации

--- src/Navigation/Navigation.js
+++ src/Navigation/Navigation.js
@@ -2,13 +2,46 @@ import React from 'react';
 import {NavLink} from 'react-router-dom';
 import style from './Navigation.module.css';
 
+const navigationData = [
+    {
+        href: '/',
+        title: 'Главная',
+        exact: true
+    },
+    {
+        href: '/competition_list',
+        title: 'Список лиг',
+        exact: false
+    },
+    {
+        href: '/team_list',
+        title: 'Список команд',
+        exact: false
+    },
+    {
+        href: '/competition_calendar',
+        title: 'Календарь лиги',
+        exact: false
+    },
+    {
+        href: '/team_calendar',
+        title: 'Календарь команды',
+        exact: false
+    },
+];
+
 function Navigation() {
     return (
         <nav>
             <ul>
-                <li>
-
-                </li>
+                {navigationData.map(
+                    item =>
+                        <li key={item.href}>
+                            <NavLink exact={item.exact} to={item.href} activeClassName={style.selected}>
+                                {item.title}
+                            </NavLink>
+                        </li>
+                )}
             </ul>
         </nav>
     );
