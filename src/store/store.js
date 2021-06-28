3f96c0f04a49d0eb0a40ef16adbaa0aed226e173
SergeyLebidko <it.sergeyler@mail.ru>
2021-06-28 17:29:21 +0300

    Удален store

--- src/store/store.js
+++
@@ -1,7 +0,0 @@
-import {createStore, combineReducers, applyMiddleware} from 'redux';
-import thunk from 'redux-thunk';
-
-
-// let store = createStore(combinedReducer, {}, applyMiddleware(thunk));
-
-// export default store;
