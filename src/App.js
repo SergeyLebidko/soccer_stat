29947be247e3429e4a0436abff7cc7d79e00c3fd
SergeyLebidko <it.sergeyler@mail.ru>
2021-06-28 11:01:16 +0300

    Добавлен компонент-заглушка для еще не созданных страниц

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import React from 'react';
+import {HashRouter, Switch, Route} from "react-router-dom";
 import './App.css';
 
 function App() {
     return (
-        <div>
-            Здесь будет контент сайта
-        </div>
+        <HashRouter>
+            <Switch>
+                <Route exact path="/" component={Cap}/>
+            </Switch>
+        </HashRouter>
     );
 }
 
 export default App;
+
+
+// Временный компонент-заглушка
+function Cap() {
+    return (
+        <div>
+            Раздел находится в разработке
+        </div>
+    )
+}
