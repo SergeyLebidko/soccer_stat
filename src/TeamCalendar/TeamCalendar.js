3f80f309eda4dff6739033fb961cdbcae99d8b57
SergeyLebidko <it.sergeyler@mail.ru>
2021-06-28 13:04:53 +0300

    Создана заготовка для компонента с календарем команды

---
+++ src/TeamCalendar/TeamCalendar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './TeamCalendar.module.css';
+
+function TeamCalendar() {
+    return (
+        <div>
+            Здесь будет страница с календарем команды
+        </div>
+    );
+}
+
+export default TeamCalendar;
