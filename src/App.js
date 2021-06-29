4ace71ca1caa0d3f90108f4c5302685ac41457b2
SergeyLebidko <it.sergeyler@mail.ru>
2021-06-29 15:38:21 +0300

    Оптимизация стилей в App.module.css

--- src/App.js
+++ src/App.js
@@ -9,7 +9,9 @@ import style from './App.module.css';
 function App() {
     return (
         <div className={style.container}>
-            <h1 className={style.site_title}>Soccer Stat</h1>
+            <h1 className={style.site_title}>
+                Soccer Stat
+            </h1>
             <Switch>
                 <Route exact path="/">
                     <Redirect to="/competitions"/>
