cbe1dddd9e72e3efd653f4e76c9e2fb363e8296b
SergeyLebidko <it.sergeyler@mail.ru>
2021-06-29 16:47:08 +0300

    В прелоадер добавлена простая анимация

--- src/Preloader/Preloader.js
+++ src/Preloader/Preloader.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import style from './Preloader.module.css';
 
-function Preloader(){
+function Preloader() {
+    let [dotCount, setDotCount] = useState(3);
+
+    // Немного оживляем прелоадер добавляя изменение количества точек после слова "Загрузка"
+    useEffect(() => {
+        let counter = dotCount;
+        let timer = setInterval(() => {
+            counter++;
+            setDotCount(counter % 5 + 1);
+        }, 300);
+        return () => clearInterval(timer);
+    });
+
     return (
         <div className={style.container}>
-            Загрузка...
+            Загрузка{new Array(dotCount + 1).join('.')}
         </div>
     );
 }
