52906ae75769b0305489680c767aa16e877a4f61
SergeyLebidko <it.sergeyler@mail.ru>
2021-07-02 19:32:13 +0300

    Переработан код обработки сетевых ошибок

--- src/utils.js
+++ src/utils.js
@@ -11,13 +11,28 @@ async function loadData(url, errorMessage) {
             }
         });
     } catch (err) {
+        // Опытным путем я выяснил, что исключение при отправке запроса возникает при слишком частом обращении к серверу,
+        // когда количество запросов превышает заданный для бесплатного тарифного плана лимит. В этом случае сервер может
+        // и не вернуть ответ со статусом 429, как это описано в документации, а попросту заблокировать запрос не отправив
+        // клиенту заголовок 'Access-Control-Allow-Origin'. Я не знаю, является ли это багом сервера или недокументированной
+        // фичей, но, в любом случае, стараюсь учитывать эту особенность API.
         throw new Error(`${errorMessage}. Возможно, превышен лимит запросов. Попробуйте обновить страницу позже. (${err.message})`);
     }
+
+    // Проверяем описанные в документации к API коды ошибок
     if (response.status === 403) throw new Error(`${errorMessage}. Данные не доступны для вашего тарифного плана.`);
     if (response.status === 404) throw new Error(`${errorMessage}. Запрошенные данные отсутствуют на сервере.`);
     if (response.status === 429) throw new Error(`${errorMessage}. Превышен лимит количества запросов. Попробуйте обновить страницу позже.`);
-    if (!response.ok) throw new Error(errorMessage);
-    return await response.json();
+
+    let json = await response.json();
+
+    // Если пришел ответ со статусом 400, то извлекаем из него сообщение сервера об ошибке и возвращаем "как есть"
+    if (response.status === 400) throw new Error(`${errorMessage}. (${json.message}).`);
+
+    // Если возникла непредвиденная ошибка другого рода - выдаем стандартное сообщение, дополненное кодом ошибки
+    if (!response.ok) throw new Error(`${errorMessage}. Код ошибки - ${response.status}`);
+
+    return json;
 }
 
 export async function loadCompetitions() {
